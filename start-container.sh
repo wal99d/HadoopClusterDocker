5de0a399b6743adb6503b7382c52b781f9ec41b3
WaL99D <WaL99D@gmail.com>
2016-11-04 16:07:29 +0300

    ..

---
+++ start-container.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# the default node number is 3
+N=${1:-3}
+
+
+# start hadoop master container
+docker rm -f hadoop-master &> /dev/null
+echo "start hadoop-master container..."
+docker run -itd \
+                --net=hadoop \
+                -p 50070:50070 \
+                -p 8088:8088 \
+                --name hadoop-master \
+                --hostname hadoop-master \
+		-v ~/Desktop/Projects/Vagrant/hadoop-cluster-docker/export:/root/prism \
+                kiwenlau/hadoop:1.0 &> /dev/null
+
+
+# start hadoop slave container
+i=1
+while [ $i -lt $N ]
+do
+	docker rm -f hadoop-slave$i &> /dev/null
+	echo "start hadoop-slave$i container..."
+	docker run -itd \
+	                --net=hadoop \
+	                --name hadoop-slave$i \
+	                --hostname hadoop-slave$i \
+	                kiwenlau/hadoop:1.0 &> /dev/null
+	i=$(( $i + 1 ))
+done
+
+# get into hadoop master container
+docker exec -it hadoop-master bash
